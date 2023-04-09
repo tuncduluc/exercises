7424e049337c62f19d94a3b62b011e9866776999
tuncduluc <tuncduluc@gmail.com>
2023-04-09 05:57:05 +0300

    exercise

---
+++ ex38/es-77/exercise.js
@@ -0,0 +1,36 @@
+const getRandomNumber = (isLogged) => {
+    return new Promise((resolve, reject) => {
+      if (isLogged) {
+        const randomNumber = Math.floor(Math.random() * 10) + 1;
+        resolve(randomNumber);
+      } else {
+        reject(new Error('User not logged in'));
+      }
+    });
+  };
+  
+  const getUserData = (inputNumber) => {
+    return new Promise((resolve, reject) => {
+      if (inputNumber > 0.5) {
+        const userData = {name: 'John', age: 24};
+        resolve(userData);
+      } else {
+        reject(new Error('Input number too low'));
+      }
+    });
+  };
+  
+  const isLogged = true;
+  
+  getRandomNumber(isLogged)
+    .then((randomNumber) => {
+      console.log(`Random number: ${randomNumber}`);
+      return getUserData(randomNumber / 10);
+    })
+    .then((userData) => {
+      console.log(`User data: ${JSON.stringify(userData)}`);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+  
